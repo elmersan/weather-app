df31d233bc22773292cb806d4e63313e4b80952d
Elmer Sanchez <elmersc11@gmail.com>
2021-08-31 22:12:02 -0500

    solution cors

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,33 +1,11 @@
 import axios from "axios";
 
-(function () {
-  var cors_api_host = "cors-anywhere.herokuapp.com";
-  var cors_api_url = "https://" + cors_api_host + "/";
-  var slice = [].slice;
-  var origin = window.location.protocol + "//" + window.location.host;
-  var open = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function () {
-    var args = slice.call(arguments);
-    var targetOrigin = /^https?:\/\/([^\\/]+)/i.exec(args[1]);
-    if (
-      targetOrigin &&
-      targetOrigin[0].toLowerCase() !== origin &&
-      targetOrigin[1] !== cors_api_host
-    ) {
-      args[1] = cors_api_url + args[1];
-    }
-    return open.apply(this, args);
-  };
-})();
-
-const baseUrl = "https://www.metaweather.com/api/location/";
-
 export const getDataCities = async (cityName = "lima") => {
-  const res = await axios.get(`${baseUrl + "search/?query=" + cityName}`);
+  const res = await axios.get(`${"/search/?query=" + cityName}`);
   return res.data; //weather of city
 };
 
 export const getWeatherDataCity = async (woeid = 418440) => {
-  const res = await axios.get(`${baseUrl + woeid}/`);
+  const res = await axios.get(`/${woeid}/`);
   return res.data; //weather data of city
 };
